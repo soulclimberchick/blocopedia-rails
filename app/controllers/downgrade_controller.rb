8f71fe2b014fa6fbc80ef62b4a8504d6890c07f4
Kira Helm <soulclimberchick@gmail.com>
2019-04-13 18:32:26 -0700

    Fixed downgrade controller and constantly disappearing application.yml file. Need to fix body text to not show as array

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
-
-RSpec.describe ChargesController, type: :controller do
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+class DowngradeController < ApplicationController
+  def create
+    if current_user.role == 'premium'
+      current_user.role = 'standard'
+      current_user.save
+      flash[:notice] = "You have been reverted to standard, #{current_user.email} ."
+      redirect_to root_path
+    else
+      flash[:notice] = "You're either an admin or already standard. No charge made."
     end
   end
-
 end
