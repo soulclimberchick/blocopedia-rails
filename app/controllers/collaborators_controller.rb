e9006a1c49684a7701a1376dfa7a49238fbffae8
Kira Helm <soulclimberchick@gmail.com>
2019-06-09 16:57:48 -0700

    partial commit. Modified collaborators controller

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -4,29 +4,26 @@ class CollaboratorsController < ApplicationController
     @user = User.find_by_email(params[:email])
     @collaborator = Collaborator.where(wiki: @wiki, user: @user).first_or_initialize
 
-  if @collaborator.persisted?
-    flash[:alert] = "Oooops! This collaborator has already been added to this wiki."
-    redirect_to edit_wiki_path(@wiki)
-  else
-    if @collaborator.save
-      flash[:notice] = "You have successfully added collaborator to your wiki."
-      redirect_to edit_wiki_path(@wiki)
+    if @collaborator.persisted?
+      flash[:alert] = "Oooops! This collaborator has already been added to this wiki."
     else
-      flash.now[:alert] = "There was an error adding the collaborator. Please try again."
-      redirect_to edit_wiki_path(@wiki)
+      if @collaborator.save
+        flash[:notice] = "You have successfully added collaborator to your wiki."
+      else
+        flash.now[:alert] = "There was an error adding the collaborator. Please try again."
+      end
     end
+    redirect_to edit_wiki_path(@wiki)
   end
-end
 
-def destroy
-  @wiki = Wiki.find(params[:wiki_id])
-  @collaborator = Collaborator.find(params[:id])
+  def destroy
+    @wiki = Wiki.find(params[:wiki_id])
+    @collaborator = Collaborator.find(params[:id])
     if @collaborator.destroy
       flash[:notice] = "Collaborator was removed from your wiki."
-      redirect_to edit_wiki_path(@wiki)
     else
       flash.now[:alert] = "There was an error removing the collaborator."
-      redirect_to edit_wiki_path(@wiki)
     end
+    redirect_to edit_wiki_path(@wiki)
   end
 end
